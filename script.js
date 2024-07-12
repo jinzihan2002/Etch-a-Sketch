958183d93e665bfa17f65f3d2ab9e93bbd79ac5a
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-12 16:00:26 +0800

    Add feature to fill squares that are dragged over

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-let gridLength = 64;
+let gridLength = 100;
+let isDragging = false;
 
 const container = document.querySelector("#container");
 
 for (let i = 0; i < gridLength; ++i) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
+
     for (let j = 0; j < gridLength; ++j) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
+
+        square.addEventListener("mousedown", () => {
+            square.className = "square colored";
+        });
+        square.addEventListener("mouseover", () => {
+            if (isDragging) {
+                square.className = "square colored";
+            }
+        });
+
         row.appendChild(square);
     }
     container.appendChild(row);
 }
+
+document.body.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    isDragging = true;
+});
+
+document.body.addEventListener("mouseup", () => {
+    isDragging = false;
+});
