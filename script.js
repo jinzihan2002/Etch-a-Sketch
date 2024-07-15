6c6cd1602a92fceedbf295249d859871b3fd07d5
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-15 09:48:30 +0800

    Add button to clear colored grid squares

--- script.js
+++ script.js
@@ -2,27 +2,30 @@ let gridLength = 100;
 let isDragging = false;
 
 const container = document.querySelector("#container");
+const btnClear = document.querySelector("#clear");
 
-for (let i = 0; i < gridLength; ++i) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
+function loadGrid() {
+    for (let i = 0; i < gridLength; ++i) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
 
-    for (let j = 0; j < gridLength; ++j) {
-        const square = document.createElement("div");
-        square.setAttribute("class", "square");
+        for (let j = 0; j < gridLength; ++j) {
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
 
-        square.addEventListener("mousedown", () => {
-            square.className = "square colored";
-        });
-        square.addEventListener("mouseover", () => {
-            if (isDragging) {
-                square.className = "square colored";
-            }
-        });
+            square.addEventListener("mousedown", () => {
+                square.setAttribute("class", "square colored");
+            });
+            square.addEventListener("mouseover", () => {
+                if (isDragging) {
+                    square.className = "square colored";
+                }
+            });
 
-        row.appendChild(square);
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
 document.body.addEventListener("mousedown", (e) => {
@@ -32,4 +35,13 @@ document.body.addEventListener("mousedown", (e) => {
 
 document.body.addEventListener("mouseup", () => {
     isDragging = false;
+});
+
+document.body.addEventListener("load", loadGrid());
+
+btnClear.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square.colored");
+    squares.forEach(square => {
+        square.setAttribute("class", "square");
+    });
 });
